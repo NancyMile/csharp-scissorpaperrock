2bc6793747e24a5b294deb82c0ed53d2c939c686
nancy <smithnancy@outlook.com>
2023-09-30 21:13:10 +1000

    Rock paper scisor, player selection

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Rock, Paper, Scissors");
+
+while(true)
+{
+    Console.WriteLine("Welcome");
+    var selectedChoice = SelectedChoice();
+    var yourChoice = char.Parse(selectedChoice);
+
+    Console.WriteLine($"You have Selected {yourChoice} ");
+}
+
+string SelectedChoice(){
+    Console.WriteLine("Please enter P,R,S");
+    var selectedChoice = Console.ReadLine();
+
+    if(selectedChoice?.ToLower() != "r" && selectedChoice?.ToLower() != "p" && selectedChoice?.ToLower() != "s")
+    {
+        Console.WriteLine("Please enter P,R,S");
+        selectedChoice = SelectedChoice();
+    }
+
+    return selectedChoice;
+}
+
