55f3ea1007558fa7083653e454a44bb5d5beb599
nancy <smithnancy@outlook.com>
2023-09-30 21:31:59 +1000

    oponent selection

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Rock, Paper, Scissors");
+Console.WriteLine("Welcome");
 
 while(true)
 {
-    Console.WriteLine("Welcome");
     var selectedChoice = SelectedChoice();
     var yourChoice = char.Parse(selectedChoice);
 
     Console.WriteLine($"You have Selected {yourChoice} ");
+
+    var machineChoice = MachineChoice();
+
+    Console.WriteLine($"Machine has chosen {machineChoice}");
 }
 
 string SelectedChoice(){
@@ -24,3 +28,13 @@ string SelectedChoice(){
     return selectedChoice;
 }
 
+
+char MachineChoice()
+{
+    char[] options = new char[] {'r','p','s'};
+
+    Random random = new Random();
+    int randomIndex = random.Next(0,options.Length);
+
+    return options[randomIndex];
+}
